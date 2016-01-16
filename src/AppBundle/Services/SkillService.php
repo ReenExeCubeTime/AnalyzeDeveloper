dec9418b619d42a0e08e29f512d2b44076833e35
ReenExe <ipourri@gmail.com>
2016-01-16 19:16:28 +0200

    test add skill

--- src/AppBundle/Services/SkillService.php
+++ src/AppBundle/Services/SkillService.php
@@ -2,10 +2,25 @@
 
 namespace AppBundle\Services;
 
+use AppBundle\Entity\SSkill;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
 class SkillService
 {
+    private $doctrine;
+
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     public function add($name)
     {
+        $skill = new SSkill();
 
+        $skill->setName($name);
+
+        $this->doctrine->getManager()->persist($skill);
+        $this->doctrine->getManager()->flush();
     }
 }
