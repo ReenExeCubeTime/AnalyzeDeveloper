cc18379b77b922f033eb6ac735e10b10f6302cfa
ReenExe <ipourri@gmail.com>
2016-01-16 18:37:50 +0200

    progress

---
+++ tests/AppBundle/Service/UserServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\AppBundle\Service;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserServiceTest extends KernelTestCase
+{
+    public function test()
+    {
+
+    }
+}
