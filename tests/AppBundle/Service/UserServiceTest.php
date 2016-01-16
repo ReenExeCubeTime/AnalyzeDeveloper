18f93a6f4f202730f63e22d0d78d781c85b3e4d4
ReenExe <ipourri@gmail.com>
2016-01-16 18:44:33 +0200

    progress

--- tests/AppBundle/Service/UserServiceTest.php
+++ tests/AppBundle/Service/UserServiceTest.php
@@ -6,6 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserServiceTest extends KernelTestCase
 {
+    private $container;
+
+    public function testIndex()
+    {
+        static::bootKernel();
+
+        $this->container = static::$kernel->getContainer();
+    }
+
     public function test()
     {
 
