d88462a82080ff365402d1460be10b64b9682ce6
ReenExe <ipourri@gmail.com>
2016-01-25 21:01:58 +0200

    progress

--- src/AppBundle/Service/DeveloperSearcherService.php
+++ src/AppBundle/Service/DeveloperSearcherService.php
@@ -3,11 +3,23 @@
 namespace AppBundle\Service;
 
 use Symfony\Component\HttpFoundation\ParameterBag;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 
 class DeveloperSearcherService implements DeveloperSearcherServiceInterface
 {
+    private $doctrine;
+
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @param ParameterBag $parameters
+     * @return \AppBundle\Entity\SDeveloperProfile[]|array
+     */
     public function search(ParameterBag $parameters)
     {
-
+        return $this->doctrine->getManager()->getRepository('AppBundle:SDeveloperProfile')->findAll();
     }
 }
--- tests/AppBundle/Service/DeveloperSearcherServiceTest.php
+++ tests/AppBundle/Service/DeveloperSearcherServiceTest.php
@@ -10,28 +10,63 @@ class DeveloperSearcherServiceTest extends AbstractServiceTest
     {
         parent::setUp();
 
-        $this->container->get('rqs.database.tester')->clear();
+        $this->createDeveloperList();
     }
 
     /**
      * @covers \AppBundle\Service\DeveloperSearcherService::search
      * @dataProvider dataProvider
      * @param array $parameters
+     * @param array $expectDeveloperProfileIdList
      */
-    public function test(array $parameters)
+    public function test(array $parameters, array $expectDeveloperProfileIdList)
     {
-        $this->getService()->search(new ParameterBag($parameters));
+        $developerProfileCollection = $this->getService()->search(new ParameterBag($parameters));
+
+        $developerProfileIdList = [];
+        foreach ($developerProfileCollection as $developerProfile) {
+            $developerProfileIdList[] = $developerProfile->getId();
+        }
+
+        $this->assertSame($developerProfileIdList, $expectDeveloperProfileIdList);
     }
 
     public function dataProvider()
     {
         yield [
-            []
+            [],
+            [1]
         ];
     }
 
     private function getService()
     {
         return $this->container->get('rqs.developer.searcher');
+    }
+
+    private function createDeveloperList()
+    {
+        $this->container->get('rqs.database.tester')->clear();
+
+        $skills = [
+            'PHP',
+            'Redis',
+            'SQL',
+            'JavaScript',
+            'TDD',
+        ];
+
+        $this->container->get('rqs.skill')->create(...$skills);
+
+        $title = 'Senior Developer';
+        $salary = 5000;
+        $description = 'All';
+
+        $developerProfile = $this->container->get('rqs.developer_profile')->create(
+            $this->getTestUser(),
+            $title,
+            $salary,
+            $description
+        );
     }
 }
